fe6192fa60a7e31c69f62e70f41c4db2ead0e150
Alex Gjorvad <alexgjorvad@gmail.com>
2018-04-27 14:11:16 -0500

    server side done

--- server/routes/koala.route.js
+++ server/routes/koala.route.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = require('express').Router();
 const mongoose = require('mongoose');
 const databaseUrl = 'mongodb://localhost:27017/kochab';
-
-// const Koala = require('../models/food.schema');
+const Koala = require('../models/koala.schema');
 
 mongoose.connect(databaseUrl);
 
@@ -17,28 +16,52 @@ mongoose.connection.on( 'error', () => {
 
 // POST
 router.post('/', (req, res) => {
-    console.log('POST to /koala req.body =', req.body);
-  });
-  
-  // GET
-  router.get('/', (req, res) => {
-    koala.find({})
-    .then(dataFromDataBase) => {
-        res.send(dataFromDataBase);
-        console.log('data from data from database', dataFromDataBase)
+    console.log('POST to /koala req.body', req.body);
+    console.log(req.body);
+    Koala.create(req.body)
+    .then((databaseResults) => {
+        //good things happened
+        res.sendStatus(200);
     })
-    .catch(error => {
-        console.log('error with koala find', error);
+    .catch((error) => {
+        console.log('error find', error);
         res.sendStatus(500);
     });
 });
+  
+  // GET
+  router.get('/', (req, res) => {
+    Koala.find({})
+    .then((dataFromDataBase) => {
+        res.send(dataFromDataBase);
+        console.log('data from data from database', dataFromDataBase)
+    })
+    .catch((error) => {
+        console.log('error with koala find', error);
+        res.sendStatus(500);
+    });
+  });
 
-router.put('/', (req, res) =>{
-//
+router.delete('/', (req, res) => {
+    Koala.findByIdAndRemove(req.query._id)
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('error from delete', error);
+            res.sendStatus(500);
+        });
 });
 
-router.delete('/', (req, res) =>{
-//
+router.put('/', (req, res) => {
+    Koala.findByIdAndUpdate(req.body._id, req.body)
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('error from put', error);
+            res.sendStatus(500);
+        });
 });
   
 module.exports = router;
