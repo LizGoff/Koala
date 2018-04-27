27e08aeaf8122e886f3731fece57244cf234e714
Alex Gjorvad <alexgjorvad@gmail.com>
2018-04-27 11:49:08 -0500

    ready to test

--- server/routes/koala.route.js
+++ server/routes/koala.route.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = require('express').Router();
-const databaseUrl = 'mongodb://localhost:27017/kochab'
+const mongoose = require('mongoose');
+const databaseUrl = 'mongodb://localhost:27017/kochab';
+
 // const Koala = require('../models/food.schema');
 
+mongoose.connect(databaseUrl);
+
+mongoose.connection.on('connected', () => {
+    console.log( `mongoose connected to ${databaseUrl}`);
+});
+
+mongoose.connection.on( 'error', () => {
+    console.log(`mongoose connection error ${error}`);
+});
+
+// POST
 router.post('/', (req, res) => {
     console.log('POST to /koala req.body =', req.body);
   });
