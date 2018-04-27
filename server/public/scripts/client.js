b663b1bc6b54b927955103f44306904bb6fff504
Jason Phan <Phanx091@umn.edu>
2018-04-27 11:51:07 -0500

    buttons and input fields

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,50 @@
-console.log( 'js' );
+console.log( 'js' );;
+
+var app = angular.module("KoalaApp", []);
+
+app.controller("KoalaController", [
+    "$http",
+    function($http) {
+        console.log("KoalaController has been loaded");
+        var self = this;
+        self.koalaList = []
+        self.newKoala = {};
+        getKoalaToDom()
+
+        function getKoalaToDom() {
+            $http({
+                method: "GET",
+                url: "/koala"
+            })
+            .then(function(response) {
+                self.koalaList = response.data;  
+            })
+            .catch(function(error) {
+                console.log("error on /koala get", error);
+            });
+        }
+
+        self.createKoala = function() {
+            console.log(self.newKoala)
+            $http({
+                method: "POST",
+                url: "/koala",
+                data: self.newKoala
+            })
+            .then(function(response) {
+                console.log('post response:', response);
+                getKoalaToDom()
+            })
+            .catch(function(error) {
+                console.log("error on /koala get", error);
+            });
+        }
+    }
+
+
+
+
+
+
+
+]);
